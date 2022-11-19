39cbe3f22b23314b0ed0db7ff3bcc898c69d2ba5
Mateusz <112409434+mat-illing@users.noreply.github.com>
2022-11-19 21:40:43 +0100

    edited sleza.js

--- assets/js/sleza.js
+++ assets/js/sleza.js
@@ -1,4 +1,4 @@
-const papa = new Date(2022, 10, 19, 21, 38, 0, 0); 
+const papa = new Date(2022, 10, 20, 21, 37, 0, 0); 
 var currentDate = new Date();
     if (currentDate > papa)
         window.location = "klip.html";
